72ef4c911cc388b6f6b1fcf5bbd9e9d6986820bb
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-05-03 01:19:56 +0300

    Initial commit

---
+++ Homework3.2.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+struct BankAccount {
+    int accountNumber;
+    std::string ownerName;
+    double balance;
+};
+
+void changeBalance(BankAccount& account, double newBalance) {
+    account.balance = newBalance;
+}
+
+int main() {
+    BankAccount account;
+    std::cout << "Введите номер счёта: ";
+    std::cin >> account.accountNumber;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> account.ownerName;
+    std::cout << "Введите баланс: ";
+    std::cin >> account.balance;
+
+    double newBalance;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newBalance;
+
+    changeBalance(account, newBalance);
+    std::cout << "Ваш счет: " << account.ownerName << "," << account.accountNumber << "," <<
+        account.balance << std::endl;
+    return 0;
+}
